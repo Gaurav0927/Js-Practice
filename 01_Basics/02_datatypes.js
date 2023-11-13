fe9fa35efa42e9941e483e47c65f006484571d43
Gaurav0927 <54946007+Gaurav0927@users.noreply.github.com>
2023-11-13 15:12:39 +0000

    datatypes intro done

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treats all js code as new version 
+
+// alert(27+10); we are using node js and this alert only works in browser
+
+// Datatypes in js
+
+// Number => 2 to the power of 53
+
+// bigInt;
+
+// String  => "";
+
+// Boolean  => true / false;
+
+// null => standalone value
+
+// undefined => abhi value assigned nahi hua hain
+
+// symbol => unique;
+
+// object
+
+let mobileBrand = "nokia";
+
+console.log(typeof mobileBrand);
+
+console.log(typeof undefined);
+
+console.log(typeof null); // this is an error of javascript when typeof is used for null
