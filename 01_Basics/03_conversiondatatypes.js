731b639c58752060200eba6f1439abab8ae6c98c
Gaurav0927 <54946007+Gaurav0927@users.noreply.github.com>
2023-11-14 12:43:51 +0000

    Conversion Of Datatypes

---
+++ 01_Basics/03_conversiondatatypes.js
@@ -0,0 +1,62 @@
+// To Number Conversion
+
+/* 
+let score = "33abc";
+
+console.log(score);
+console.log(typeof score);
+
+let valueInNumber = Number(score);
+
+console.log(typeof valueInNumber);
+
+console.log(valueInNumber);
+
+
+// "33" => 33 gets succesfully converted into a number whereas 33abc gives Nan
+
+// true => 1 true gets cobverted into 1
+
+// false => 0 false gets cobverted into 0
+
+*/
+
+// Convert Number into Boolean
+
+/* 
+
+let randomData = "Surat";
+
+console.log(typeof randomData);
+
+let boolanConverted = Boolean(randomData);
+
+console.log(typeof boolanConverted);
+
+console.log(boolanConverted);
+
+// 1 => true
+
+// 0 => False
+
+// "India" => True
+
+// "" => False
+
+*/
+
+// Convert Number To String
+
+let randomNumber = undefined;
+
+console.log(randomNumber);
+
+console.log(typeof randomNumber);
+
+let stringConverter = String(randomNumber);
+
+console.log(stringConverter);
+
+console.log(typeof stringConverter);
+
+
