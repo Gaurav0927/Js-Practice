9618f41c889c9839a7b0611e03c4bf33ca8c8b5d
Gaurav0927 <54946007+Gaurav0927@users.noreply.github.com>
2023-11-14 13:42:08 +0000

    comparison operator in js

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,28 @@
+//  Equity opeator ==
+
+// Comparison Opeator >=, <=, == 
+// comparison operator treats null as 0
+
+// console.log(null >= 0);
+
+// console.log(null == 0);
+
+// console.log(null === 0);
+
+// console.log(null > 0);
+
+// Undefined always gives false when used with comparison or equity opertor
+
+// console.log(undefined > 0);
+
+// console.log(undefined >= 0);
+
+// console.log(undefined === 0);
+
+// console.log(null >= undefined);
+
+// Strict Comparison ===
+
+console.log("456" === 456); // this checks and prevents the conversion of string "456" into number
+
+console.log("456" == 456);
