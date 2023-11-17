8cd970d5e4ecaff17943e76a7a8d299fdb91dd78
Gaurav0927 <54946007+Gaurav0927@users.noreply.github.com>
2023-11-17 15:57:16 +0000

    strings in js

---
+++ 01_Basics/05_strings.js
@@ -0,0 +1,51 @@
+// const userName = "Gaurav";
+
+// const repoCount = 45;
+
+// console.log(`Hello my name is ${userName} and my repo count is ${repoCount}`);
+
+// Another way of describing string
+
+let myName = new String("Mayank");
+
+// console.log(myName);
+
+// console.log(myName.length);
+
+// console.log(myName.toUpperCase());
+
+// console.log(myName.charAt(2));
+
+// console.log(myName.indexOf("k"));
+
+// In sub string we can't give negative value while in string it is allowed
+
+// let otherName = myName.substring(0,3)
+
+// let anotherName = myName.slice(-5,1)
+
+// console.log(otherName);
+
+// console.log(anotherName);
+
+// Trim is used to remove the extra space
+
+// let myCountry = "      India               ";
+// console.log(myCountry);
+// console.log(myCountry.trim());
+
+// // replace method
+
+// let myUrl = "https://www.google.com/google%145562india"
+
+// console.log(myUrl);
+
+// console.log(myUrl.replace("%145562","_"));
+
+// We can covert string into array using the split
+
+const myState = "My-State-Is-Maharashtra";
+
+console.log(myState);
+
+console.log(myState.split("-"));
